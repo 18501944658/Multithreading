d359c699b4896ea9e059f8b78e2892df015b1a8a
lijinjian <18501944658@163.com>
2020-12-10 22:57:17 +0800

    2

--- src/main/java/com/itszt/pool/TestThreadPool.java
+++ src/main/java/com/itszt/pool/TestThreadPool.java
@@ -68,13 +68,18 @@ public class TestThreadPool {
                 (runnable) -> {
                     return new ThreadFactoryPool("t" + atomicInteger.incrementAndGet()).newThread(runnable);
                 }, new ThreadPoolExecutor.CallerRunsPolicy());
+
         AtomicInteger y = new AtomicInteger(0);
 
-        for (int i = 0; i < 5; i++) {
-            log.debug("当前开始执行任务{}",(i+1));
+        for (int i = 1; i <=5; i++) {
+            log.debug("当前开始执行任务{}",(i));
             threadPoolExecutor.execute(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(1000000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 log.debug("当前线程{}正在执行任务-{}", Thread.currentThread().getName(), y.incrementAndGet());
-//                System.out.println("当前正在执行任务-----{}" + y.incrementAndGet());
             });
         }
 
